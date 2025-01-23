f449d2974ae827e834298eca168fbe2cfd03d2d8
Henrick <ricknogueira1231@gmail.com>
2025-01-23 16:57:22 -0300

    first commit

---
+++ app/src/main/java/com/hkprogrammer/meuprimeiroprojeto/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.hkprogrammer.meuprimeiroprojeto
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun sortear(view: View) {
+        val textoResultado = findViewById<TextView>(R.id.text_resultado)
+        val numero = Random().nextInt(11)
+        textoResultado.setText("número gerado $numero")
+    }
+
+}
